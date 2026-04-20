bfdbde1db2a30698f3d7682b2312376a866e7c80
SASTEKA A <175807549+SASTEKAZHAGESAN@users.noreply.github.com>
2026-04-20 21:46:33 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,8 +2,17 @@ function login() {
   let user = document.getElementById("name").value;
   let pass = document.getElementById("password").value;
 
-  // 👇 You define your own login
-  if (user === "karthika" && pass === "12345") {
+  // 👇 Multiple users (you can add more)
+  let users = [
+    { name: "karthika", password: "12345" },
+    { name: "ravi", password: "11111" },
+    { name: "kumar", password: "22222" }
+  ];
+
+  // Check user
+  let validUser = users.find(u => u.name === user && u.password === pass);
+
+  if (validUser) {
     alert("Login successful!");
     window.location.href = "index.html";
   } else {
